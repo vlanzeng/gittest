d4f3f68504933840776e53dd6e56df1ea6ed31d2
zengwei <wzbbbx@sina.com>
2015-07-20 23:43:58 +0800

    加入汉语

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
 public class Hello{
 	
   public static void main(String[]args){
-
-
+	System.out.println("hello,world!!");
+	System.out.println("ddd");	
+        System.out.println("dddeee");
+System.out.println("dddeee");
+System.out.println("dddeee");
+System.out.println("我回来了！！");
 }
-
 }
