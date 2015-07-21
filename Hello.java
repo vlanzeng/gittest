0796eee450b1898c419c7e45ec9677f10cc755bc
zengwei <wzbbbx@sina.com>
2015-07-21 14:44:29 +0800

    分支冲突

--- Hello.java
+++ Hello.java
@@ -10,7 +10,10 @@ System.out.println("我回来了！！");
 System.out.println("我回来了！！2");
 System.out.println("我回来了！！3");
 System.out.println("我回来了！！4");
-System.out.println("我回来了！！5 住干");
 System.out.println("我回来了！！住干");
+
+System.out.println("我回来了！！6住干");
+System.out.println("我回来了！！7住干");
+
 }
 }
