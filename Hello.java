0355837e848ffcd7cf54710888421311b2390e6c
zengwei <wzbbbx@sina.com>
2015-07-23 16:03:17 +0800

    1修改H文件

--- Hello.java
+++ Hello.java
@@ -1,19 +1,10 @@
 ﻿public class Hello{
 	
   public static void main(String[]args){
-	System.out.println("hello,world!!");
-	System.out.println("ddd");	
-        System.out.println("dddeee");
-System.out.println("dddeee");
-System.out.println("dddeee");
-System.out.println("我回来了！！");
-System.out.println("我回来了！！2");
-System.out.println("我回来了！！3");
-System.out.println("我回来了！！4");
-System.out.println("我回来了！！住干");
-
-System.out.println("我回来了！！6住干");
-System.out.println("我回来了！！7住干");
-
-}
+	doSomething();
+	
+ }
+public static void  doSomething(){
+		System.out.println("Happy,less!!");	
+	}
 }
